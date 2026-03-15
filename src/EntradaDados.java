6eeaca6cdd7a084fb266564329caeb9fe0fda7d3
Pedro Henrique <pedro2006batista@gmail.com>
2026-03-15 02:04:30 -0300

    update arquivos

--- src/EntradaDados.java
+++ src/EntradaDados.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class EntradaDados {
-    public static void main (String [] args) {
+    public static void main (String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("digite um texto:");
         String texto = sc.nextLine();
--- src/Ex/Exercicio1.java
+++ src/Ex/Exercicio1.java
@@ -1,7 +1,15 @@
 package Ex;
+import java.util.Scanner;
 
 public class Exercicio1 {
-    public static void main(String [] Args){
+    public static void main(String[] Args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("digite o raio do circulo: ");
+        double raio = sc.nextDouble();
+        double pi = 3.14159;
+        double area = pi * (raio * raio);
+        System.out.printf("a area do seu circulo é %.2f", area);
 
+        sc.close();
     }
 }
