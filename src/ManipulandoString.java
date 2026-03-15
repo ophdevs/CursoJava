e39194756aa9098bcefb840fce91942c9ccef9dc
Pedro Henrique <pedro2006batista@gmail.com>
2026-03-15 14:00:07 -0300

    manipulacao de strings

---
+++ src/ManipulandoString.java
@@ -0,0 +1,8 @@
+public class ManipulandoString {
+    public static void main (String[] args){
+        String texto = "abc 2342 CSDFE AMA avcv";
+        String[] vect = texto.split(" ");
+        System.out.println(vect[0]);
+    }
+
+}
