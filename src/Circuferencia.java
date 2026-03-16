e7fd41ba0e5655890661514511584b17769240bf
Pedro Henrique <pedro2006batista@gmail.com>
2026-03-16 12:05:18 -0300

    update circuferencia e calculator

--- src/Circuferencia.java
+++ src/Circuferencia.java
@@ -8,17 +8,17 @@ public class Circuferencia {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        calculator calc = new calculator();
+
 
         System.out.print("digite o raio do triangulo: ");
         double r = sc.nextDouble();
 
-        double c = calc.Circunfe(r);
-        double v = calc.Volume(r);
+        double c = calculator.Circunfe(r);
+        double v = calculator.Volume(r);
 
         System.out.printf("cicuferencia: %.2f %n", c);
         System.out.printf("Volume: %.2f %n", v);
-        System.out.printf("pi:  %.2f %n", calc.PI);
+        System.out.printf("pi:  %.2f %n", calculator.PI);
 
         sc.close();
 
