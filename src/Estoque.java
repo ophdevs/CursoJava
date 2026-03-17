e0c58103fd244a70439124af392b89740c79e792
Pedro Henrique <pedro2006batista@gmail.com>
2026-03-17 20:09:02 -0300

    update estoque

--- src/Estoque.java
+++ src/Estoque.java
@@ -5,14 +5,15 @@ import java.util.Scanner;
 public class Estoque {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Produto item = new Produto();
 
         System.out.print("Digite o nome do produto: ");
-        item.nome = sc.nextLine();
-        System.out.print("Digite o preço do produto: ");
-        item.preco = sc.nextDouble();
+        String nome = sc.nextLine();
+        System.out.print("Di.gite o preço do produto: ");
+        double preco = sc.nextDouble();
         System.out.print("Digite a quantidade do produto: ");
-        item.quantidade = sc.nextInt();
+        int quantidade = sc.nextInt();
+
+        Produto item = new Produto(nome, preco ,quantidade);
 
         item.MostrarProdutos();
 
