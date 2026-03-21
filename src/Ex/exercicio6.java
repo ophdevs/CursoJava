fd27232b98d80fc0c7e69aac5e5adb47aa8bc3ca
Pedro Henrique <pedro2006batista@gmail.com>
2026-03-21 18:00:09 -0300

    exercicio 6 terminado

--- src/Ex/exercicio6.java
+++ src/Ex/exercicio6.java
@@ -6,18 +6,30 @@ public class exercicio6 {
     static void main() {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("quantos numeros quer digitar: ");
+        System.out.print("quantos numeros quer digitar: ");
         int n = sc.nextInt();
         int number [] = new int[n];
 
         int soma = 0;
 
         for(int i = 0; i < n; i++){
-            System.out.println("Digite um numero: ");
+            System.out.print("Digite um numero: ");
             number [i] = sc.nextInt();
             soma += number[i];
-
         }
+        System.out.print("Valores = ");
+        for (int i = 0; i < n; i++){
+            System.out.print(" " + number [i]);
+        }
+
+
+        int media = soma / n;
+        System.out.println(" ");
+        System.out.printf("soma : %d%n", soma);
+        System.out.printf("media : %d%n",media);
+
+
+
 
 
     }
