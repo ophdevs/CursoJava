a1f6776b76b140d4648a6cf2801b30fb591f48a5
Pedro Henrique <pedro2006batista@gmail.com>
2026-03-27 22:28:12 -0300

    update Listas

--- src/Ex/Exercicio9Listas.java
+++ src/Ex/Exercicio9Listas.java
@@ -18,7 +18,14 @@ public class Exercicio9Listas {
             System.out.print("Valor: ");
             double valor = sc.nextDouble();
         }
-
+        for (int i = 0; i < quantProd; i++){
+            System.out.print("ID: ");
+            int id = sc.nextInt();
+            System.out.print("Nome: ");
+            String name = sc.nextLine();
+            System.out.print("Valor: ");
+            double valor = sc.nextDouble();
+        }
 
     }
 }
