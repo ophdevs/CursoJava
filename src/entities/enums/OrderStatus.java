caca9e60b3b81c88c920be355f51b2c0a6588491
Pedro Henrique <pedro2006batista@gmail.com>
2026-05-11 19:34:00 -0300

    Enum e entities

---
+++ src/entities/enums/OrderStatus.java
@@ -0,0 +1,8 @@
+package entities.enums;
+
+public enum OrderStatus {
+    PROCESSANDO_PAGAMENTO,
+    PROCESSANDO,
+    ENVIADO,
+    ENTREGUE;
+}
---
+++ src/util/EnumTeste.java
@@ -0,0 +1,13 @@
+package util;
+
+import entities.Order;
+import entities.enums.OrderStatus;
+import java.util.Date;
+
+public class EnumTeste {
+    static void main() {
+        Order order = new Order(4050, new Date(), OrderStatus.PROCESSANDO);
+
+        System.out.println(order);
+    }
+}
