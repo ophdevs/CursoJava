4cd37f5198fb1370177ebaf90c85e87b51c28a55
Pedro Henrique <pedro2006batista@gmail.com>
2026-05-13 12:13:41 -0300

    converter horario para string

--- src/DateTIme.java
+++ src/DateTIme.java
@@ -1,16 +1,18 @@
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
 public class DateTIme {
     static void main(String[] args) {
-        DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd-MM-yyyy"); // criar formatacao
         DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
+        DateTimeFormatter fmt3 = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm").withZone(ZoneId.systemDefault()); // formatacao data e hora com fuso horario
 
-        LocalDate ds1 = LocalDate.now();
-        LocalDateTime ds2 = LocalDateTime.now();
-        Instant ds3 = Instant.now();
+        LocalDate ds1 = LocalDate.now(); //data atual da acao
+        LocalDateTime ds2 = LocalDateTime.now(); // data e hora atual da ação
+        Instant ds3 = Instant.now(); // data e hora exata com fuso idependente de onde roda
 
         LocalDate ds4 = LocalDate.parse("14-01-2026", fmt1);
         LocalDate ds5 = LocalDate.parse("14-01-2026 01:30", fmt2);
@@ -23,8 +25,8 @@ public class DateTIme {
         System.out.println("ds3 = " + ds3);
         System.out.println("ds4 = " + ds4);
         System.out.println("ds5 = " + ds5);
-        System.out.println("ds6 = " + ds6);
-        System.out.println("ds7 = " + ds7);
+        System.out.println("ds6 = " + ds6.format(fmt1)); //formatar para string
+        System.out.println("ds7 = " + fmt2.format(ds7)); // outra forma de formatar
 
 
 
