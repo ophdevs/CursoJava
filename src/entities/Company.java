63179fda886ee84be7753f9133face8feff13099
Pedro Henrique <pedro2006batista@gmail.com>
2026-05-26 10:00:00 -0300

    estudos herança: Company, Individual e exercício fixação POO

---
+++ src/entities/Company.java
@@ -0,0 +1,31 @@
+package entities;
+
+public class Company  extends TaxPayer{
+    public Integer numberOfEmployees;
+
+    public Company(Integer numberOfEmployees) {
+        this.numberOfEmployees = numberOfEmployees;
+    }
+
+    public Company(String name, Double anuallncome, Integer numberOfEmployees) {
+        super(name, anuallncome);
+        this.numberOfEmployees = numberOfEmployees;
+    }
+
+    public Integer getNumberOfEmployees() {
+        return numberOfEmployees;
+    }
+
+    public void setNumberOfEmployees(Integer numberOfEmployees) {
+        this.numberOfEmployees = numberOfEmployees;
+    }
+
+    @Override
+    public Double tax() {
+        if (numberOfEmployees > 10){
+            return anuallncome * 0.14;
+        } else {
+            return anuallncome * 0.16;
+        }
+    }
+}
