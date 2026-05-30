c7b327ce235bffd6d644587d09e55beb5d3e98df
Pedro Henrique <pedro2006batista@gmail.com>
2026-05-29 23:51:16 -0300

    fix: corrigir erros de estrutura no exercicio de fatorial recursivo

--- src/recursividade/fatorial.java
+++ src/recursividade/fatorial.java
@@ -1,5 +1,7 @@
 package recursividade;
 
+import java.util.Scanner;
+
 public class fatorial {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -9,18 +11,10 @@ public class fatorial {
         sc.close();
     }
 
-}
-public static int calculoFatorial( int n){
-    if (n == 0){
-        return 1;
+    public static int calculoFatorial(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        return n * calculoFatorial(n - 1);
     }
-    return n* calculoFatorial(n - 1);
-}
-
-static void main() {
-    Scanner sc = new Scanner(System.in);
-    System.out.println("digite um numero: ");
-    int numero = sc.nextInt();
-    System.out.println(calculoFatorial(numero));
-    sc.close();
 }
