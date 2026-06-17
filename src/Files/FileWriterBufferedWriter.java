1f7d60f3955d047e1efda6a22c3f26b843e6b669
Pedro Henrique <pedro2006batista@gmail.com>
2026-06-17 20:17:46 -0300

    update Files

---
+++ src/Files/FileWriterBufferedWriter.java
@@ -0,0 +1,24 @@
+package Files;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileWriterBufferedWriter {
+    static void main(String[] args) {
+        String[] lines = new String[] {"Good morning", "Good afternoon", "Good night"};
+
+        String path = "/home/ophdev/projeto git/CursoJava/src/Files/dados.txt";
+
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))){
+            for (String line : lines){
+                bw.write(line);
+                bw.newLine();
+            }
+
+        }
+        catch (IOException e){
+            System.out.printf("Error : %s", e.getMessage());
+        }
+    }
+}
