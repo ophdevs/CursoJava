5a790cf64deae7888e3ac90178172b3357de70b2
Pedro Henrique <pedro2006batista@gmail.com>
2026-06-17 14:19:56 -0300

    Create archives FileReader and BufferedReader

---
+++ src/Files/LerArquivos.java
@@ -0,0 +1,24 @@
+package Files;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class LerArquivos {
+    static void main() {
+        File file = new File("/home/ophdev/projeto git/CursoJava/src/Files/int.txt");
+        Scanner sc = null;
+        try{
+            sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                System.out.println(sc.nextLine());
+            }
+        }catch (IOException e){
+            System.out.println("Error: " + e.getMessage());
+        }finally {
+            if (sc != null){ //verificar se o sc esta null
+                sc.close();
+            }
+        }
+    }
+}
